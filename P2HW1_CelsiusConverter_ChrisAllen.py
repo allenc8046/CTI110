b8a242a9c68539b41170f1ddb6718580078375c1
allenc8046 <43551372+allenc8046@users.noreply.github.com>
2018-09-24 18:24:04 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter_ChrisAllen.py
@@ -0,0 +1,21 @@
+# Celsius to Farenheit Converter
+# 24 Sep 2018
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter 
+# Chris Allen
+#
+# determine the temperature in Celsius
+
+celsius = int(input("What is temperature in Celsius: "))
+
+#convert Celsius to Farenheit
+
+farenheit = 9 / 5 * celsius + 32
+
+# display temperature in Farenheit
+
+print ("The temperature in Farenheit is: ", farenheit)
+
+
+
+
+                                  
---
+++ P2HW2_MaleFemale_Percentage_ChrisAllen.py
@@ -0,0 +1,32 @@
+# Determining the percentage of males and females in a class
+# 24 Sep 2018
+# CTI-110 P2HW2 - Male Female Percentage
+# Chris Allen
+#
+# determine the total & numbers of males and females in the class
+
+totalClass = int(input("How many are in the class: "))
+
+male = int(input("How many are males: "))
+
+female = int(input("How many are females: "))
+
+# determine the pecentages
+
+percentMale = male / totalClass
+
+percentFemale = female / totalClass
+
+# display the percentage of males and females in the class
+
+print ("The percentage of the class that is male: ", format(percentMale,".0%"))
+                                                            
+print ("The percentage of the class that is female: ", format(percentFemale,".0%"))
+              
+
+
+
+
+
+
+                                  
