099ce09a0dfbc14c77b9010b7f8875a1ab2a9c0a
allenc8046 <43551372+allenc8046@users.noreply.github.com>
2018-09-24 14:35:32 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ChrisAllen.py
@@ -0,0 +1,18 @@
+# Calculating profit from total sales
+# 24 Sep 2018
+# CTI-110 P2T1 - Sales Prediction
+# Chris Allen
+#
+# determine the projected total sales
+
+totalSales = float(input("Enter the projected sales: "))
+
+# calculate the profit as 23% of total sales
+
+profit = totalSales * 0.23
+
+# display the profit
+
+print ("The profit is $", format (profit, ",.2f"))
+
+                                  
